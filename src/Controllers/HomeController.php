181c1acd11278b77646fe54b47b860c4a636ef6a
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-27 14:11:01 +0300

    add guzzle example

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,27 +1,15 @@
 <?php
 namespace App\Controllers;
 
-use App\Models\Post;
-use PDO;
-use PDOException;
+
+use GuzzleHttp\Client;
+
 class HomeController {
     public function index() {
-        try {
-            $conn = new PDO("sqlite:" . __DIR__ . '/../../db.sqlite');
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $conn->prepare("SELECT * FROM posts");
-            $stmt->execute();
-
-            // set the resulting array to associative
-            $stmt->setFetchMode(PDO::FETCH_CLASS, Post::class);
-            $result = $stmt->fetchAll();
-            var_dump($result[0]->getExcerpt());
-
-        } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
+        $client = new Client(['verify'=> false]);
+        $response = $client->get('https://rickandmortyapi.com/api/character/2');
+        $char = json_decode($response->getBody()->getContents());
+        var_dump($char->name);
     }
 
 }
